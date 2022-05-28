35da0d5c49fb9ed3e60cee1faa3f4841fe73c58c
Oniel1721 <oni1san7alv21@gmail.com>
2022-05-28 16:20:22 -0400

    shop layout created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
+import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
+import { ShopLayout } from '../components/layouts'
 
 const Home: NextPage = () => {
   return (
-    <h1>Teslo shop</h1>
+    <ShopLayout title='Teslo-Shop - Home' pageDescription='Encuentra los mejores productos de Teslo aquí' >
+      <Typography variant='h1' component='h1'>
+        Teslo shop
+      </Typography>
+      <Typography variant='h2' sx={{
+        mb: 1
+      }}>
+        Todos los productos
+      </Typography>
+    </ShopLayout>
   )
 }
 
