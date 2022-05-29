816d73447c784963381aa827b0594938cb5cb12e
Oniel1721 <oni1san7alv21@gmail.com>
2022-05-29 09:33:40 -0400

    cartList created

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,7 +1,32 @@
+import { Typography, Grid, Card, CardContent, Divider, Box, Button } from '@mui/material'
+import { CartList } from '../../components/cart'
+import { ShopLayout } from '../../components/layouts'
 
 const CartPage = () => {
   return (
-    <div>CartPage</div>
+    <ShopLayout title='Carrito - 3' pageDescription='Carrito de compras de la tienda'>
+      <Typography variant='h1' component='h1'>Carrito</Typography>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={7}>
+          <CartList />
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={5}>
+          <Card className='summary-card'>
+            <CardContent>
+              <Typography variant='h2'>Orden</Typography>
+              <Divider sx={{ my: 1 }}/>
+              {/* Order Summary */}
+              <Box sx={{ mt: 3 }}>
+                <Button color='secondary' className='circular-btn' fullWidth>
+                  Checkout
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </ShopLayout>
   )
 }
 
