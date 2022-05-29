82e7023b24b0b03ea81edb09f38efdefcf66c4e0
Oniel1721 <oni1san7alv21@gmail.com>
2022-05-29 12:44:05 -0400

    search endpoint created

---
+++ pages/api/search/[query].ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database'
+import { IProduct } from '../../../interfaces'
+import { Product } from '../../../models'
+
+type Data =
+    | { message: string }
+    | IProduct[]
+
+const searchProductsByQuery = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  let { query = '' } = req.query
+
+  if (query.length === 0) return res.status(400).json({ message: 'Debe de especificar la búsqueda' })
+  query = query.toString().toLowerCase()
+
+  await db.connect()
+  const products = await Product.find({
+    $text: { $search: query }
+  }).lean()
+  await db.disconnect()
+  return res.status(200).json(products)
+}
+
+export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
+  switch (req.method) {
+    case 'GET':
+      return searchProductsByQuery(req, res)
+    default:
+      return res.status(400).json({
+        message: 'Bad request'
+      })
+  }
+}
