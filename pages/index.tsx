c5f6ed51a3f91aa7dbaff5829f786f3f33da484d
Oniel1721 <oni1san7alv21@gmail.com>
2022-06-03 14:18:45 -0400

    swr added to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,28 @@ import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import { ShopLayout } from '../components/layouts'
 import { ProductList } from '../components/products'
-import { initialData } from '../database/products'
 import { IProduct } from '../interfaces'
 
-const Home: NextPage = () => {
+import useSWR from 'swr'
+
+const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
+
+const HomePage: NextPage = () => {
+  const { data, error } = useSWR('/api/products', fetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+
+  console.log(data)
+
   return (
     <ShopLayout title='Teslo-Shop - Home' pageDescription='Encuentra los mejores productos de Teslo aquí' >
       <Typography variant='h1' component='h1'>Teslo shop</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos </Typography>
 
-      <ProductList products={initialData.products as IProduct[]} />
+      <ProductList products={data as IProduct[]} />
     </ShopLayout>
   )
 }
 
-export default Home
+export default HomePage
