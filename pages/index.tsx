dba958fe9d9c492825c4c94ff94f3aef30c0e4e3
Oniel1721 <oni1san7alv21@gmail.com>
2022-06-03 14:30:49 -0400

    useProduct hooks and swr config

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,22 @@ import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import { ShopLayout } from '../components/layouts'
 import { ProductList } from '../components/products'
-import { IProduct } from '../interfaces'
-
-import useSWR from 'swr'
-
-const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
+import { useProducts } from '../hooks'
 
 const HomePage: NextPage = () => {
-  const { data, error } = useSWR('/api/products', fetcher)
-
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
-
-  console.log(data)
+  const { products, isLoading } = useProducts('/products')
 
   return (
     <ShopLayout title='Teslo-Shop - Home' pageDescription='Encuentra los mejores productos de Teslo aquí' >
       <Typography variant='h1' component='h1'>Teslo shop</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos </Typography>
 
-      <ProductList products={data as IProduct[]} />
+      {
+        isLoading
+          ? <h1>Cargando...</h1>
+          : <ProductList products={products} />
+      }
+
     </ShopLayout>
   )
 }
