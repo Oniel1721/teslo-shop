1e40da6fff6ad5aa453e8dff8f65133e74a7cb76
Oniel1721 <oni1san7alv21@gmail.com>
2022-06-08 15:15:28 -0400

    app providers updated

--- context/index.tsx
+++ context/index.tsx
@@ -1,16 +1,31 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import React from 'react'
+import { SWRConfig } from 'swr'
 import { FC } from '../interfaces'
+import { lightTheme } from '../themes'
 import { AuthProvider } from './auth'
 import { CartProvider } from './cart'
 import { UIProvider } from './ui'
 
 export const AppProviders:FC = ({ children }) => {
   return (
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
       <AuthProvider>
         <CartProvider>
             <UIProvider>
-              {children}
+              <ThemeProvider theme={lightTheme}>
+                <CssBaseline />
+                {
+                    children
+                }
+              </ThemeProvider>
           </UIProvider>
         </CartProvider>
       </AuthProvider>
+    </SWRConfig>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AppProviders } from '../context'
-import { ThemeProvider } from '@emotion/react'
-import { CssBaseline } from '@mui/material'
-import { lightTheme } from '../themes'
-import { SWRConfig } from 'swr'
 
 function MyApp ({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig
-      value={{
-        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-      }}
-    >
-      <AppProviders>
-        <ThemeProvider theme={lightTheme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </AppProviders>
-    </SWRConfig>
+    <AppProviders>
+      <Component {...pageProps} />
+    </AppProviders>
 
   )
 }
